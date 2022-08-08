83ef566cb7cfdd0007b825f851100a75500eae7f
Dmitriy Bykhov <reactorbd@gmail.com>
2022-08-08 21:11:16 +0300

    create Gradle project

---
+++ src/main/java/com/example/restaurantadvisor/RestaurantAdvisorApplication.java
@@ -0,0 +1,13 @@
+package com.example.restaurantadvisor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantAdvisorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestaurantAdvisorApplication.class, args);
+    }
+
+}
