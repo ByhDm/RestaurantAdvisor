036f474ace85b2fd379295b3086192f5a8e70e0d
Dmitriy Bykhov <reactorbd@gmail.com>
2022-08-08 21:55:14 +0300

    added entity Review

--- src/main/java/com/example/restaurantadvisor/entity/Review.java
+++ src/main/java/com/example/restaurantadvisor/entity/Review.java
@@ -1,4 +1,17 @@
 package com.example.restaurantadvisor.entity;
 
+import lombok.*;
+
+@Getter
+@Setter
+@EqualsAndHashCode
+@ToString
+@NoArgsConstructor
 public class Review {
+
+    private Long id;
+    private Long restaurant_id;
+    private String review;
+    private Integer rating;
+
 }
