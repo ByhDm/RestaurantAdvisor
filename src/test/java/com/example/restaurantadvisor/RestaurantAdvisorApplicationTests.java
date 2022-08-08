519d8a3f39e099f9e20153f62b7916c33dfc8d19
Dmitriy Bykhov <reactorbd@gmail.com>
2022-08-08 23:51:06 +0300

    added tests group

--- src/test/java/com/example/restaurantadvisor/RestaurantAdvisorApplicationTests.java
+++ src/test/java/com/example/restaurantadvisor/RestaurantAdvisorApplicationTests.java
@@ -2,9 +2,13 @@ package com.example.restaurantadvisor;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
-class RestaurantAdvisorApplicationTests {
+@SpringBootTest(classes = {
+        RestaurantAdvisorApplication.class,
+        H2TestProfileJPAConfig.class})
+@ActiveProfiles("test")
+public class RestaurantAdvisorApplicationTests {
 
     @Test
     void contextLoads() {
